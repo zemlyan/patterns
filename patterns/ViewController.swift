43d862affeb28899ef0d8cdebe941b6078802d86
Oleksiy Yelnikov <oleksiiyelnykov@macbook-air-oleksii.local>
2019-01-04 21:50:08 +0200

    Initial Commit

---
+++ patterns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  patterns
+//
+//  Created by Oleksii Yelnykov on 04.01.2019.
+//  Copyright © 2019 Oleksii Yelnykov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
